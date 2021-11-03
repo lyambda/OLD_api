eeff4f1dd4be562c11b7200e30e3b942afe8d8bf
Ku6iKRu6Ika <ku6ikru6ika@gmail.com>
2021-11-03 13:26:47 +0400

    Fixed encoding

--- app.py
+++ app.py
@@ -3,17 +3,23 @@ from lyambda import API
 from config import Config
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
 api = API(
     mongodb=Config['mongodb'],
     smtp=Config['smtp']
 )
 
 @app.route('/<method>', methods=["GET", "POST"])
-def main(method):
+def methods(method):
     if method not in api.methods.keys():
-        return jsonify({'ok' : False, 'description' : 'Method not found'})
+        return jsonify({'ok' : False, 'error_code' : 501, 'description' : 'Method not found'})
     else:
         return jsonify(api.methods[method](**request.args.to_dict()))
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return jsonify({'ok' : False, 'error_code' : 500})
 
 if __name__ == '__main__':
     app.run(
