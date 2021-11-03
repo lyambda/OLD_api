cfb1e6770462ca36381ed09f322751e7a405770c
Ku6iKRu6Ika <ku6ikru6ika@gmail.com>
2021-11-03 21:38:13 +0400

    Heroku

--- config.py
+++ config.py
@@ -3,3 +3,9 @@ import os
 
 with open(os.path.join(os.path.dirname(__file__), 'config.json'), encoding='utf-8') as f:
     Config = json.load(f)
+
+is_heroku = os.environ.get('HEROKU', False)
+
+if is_heroku:
+    Config['server']['host'] = '0.0.0.0'
+    Config['server']['port'] = int(os.environ.get('PORT', 5000))
