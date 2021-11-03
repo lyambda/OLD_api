70388613dcc84301aa3ad0b768d58aa6f0410550
Ku6iKRu6Ika <ku6ikru6ika@gmail.com>
2021-11-03 13:43:18 +0400

    Removed get_id

--- lyambda/utils.py
+++ lyambda/utils.py
@@ -4,22 +4,4 @@ import string
 def generate_token():
     return ''.join(random.sample(string.ascii_letters + string.digits, 32))
 
-def get_id(db, name):
-    collection = db.ids
-
-    if collection.find_one({'_id': name}) is None:
-        document = {
-            '_id': name,
-            'value' : 1
-        }
-
-        collection.insert_one(document)
-    else:
-        document = collection.find_and_modify(
-            query={'_id': name},
-            update={'$inc' : {'value' : 1}},
-            new=True
-        )
-
-    return document['value']
 
