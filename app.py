86ca66f14d59768e058a36d75b770c6541f0135a
Ku6iKRu6Ika <ku6ikru6ika@gmail.com>
2021-11-05 15:38:06 +0400

    Converted to POST

--- app.py
+++ app.py
@@ -11,18 +11,22 @@ api = API(
     smtp=Config['smtp']
 )
 
-@app.route('/<method>', methods=["GET", "POST"])
+@app.route('/<method>', methods=['POST'])
 def methods(method):
     if method not in api.methods.keys():
         abort(404)
     else:
-        data, code =  api.methods[method](**request.args.to_dict())
+        data, code =  api.methods[method](**request.form.to_dict())
 
     return jsonify(data), code, {'Content-Type': 'application/json'}
 
 @app.errorhandler(404)
 def not_found(e):
     return jsonify({'ok' : False, 'error_code' : 404, 'description' : 'Method not found'}), 404
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return jsonify({'ok' : False, 'error_code' : 405, 'description' : 'Method Not Allowed'}), 405
 
 @app.errorhandler(500)
 def internal_server_error(e):
